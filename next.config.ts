e9f55e966aa69b7c0949d039c13d71738d36751b
John Drigalla <johndylan.drigalla@otto.de>
2026-05-23 03:41:55 +0200

    edit next config ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
 const nextConfig: NextConfig = {
     output: "export",
     images: {
         unoptimized: true,
     },
-    basePath: isGithubPages ? "/JohnManjeetSeed" : "",
-    assetPrefix: isGithubPages ? "/JohnManjeetSeed/" : "",
+    basePath: "/JohnManjeetSeed",
+    assetPrefix: "/JohnManjeetSeed/",
     trailingSlash: true,
 };
 
