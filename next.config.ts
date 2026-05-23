4a68b48da8aed67746034734daf24e226cb8ebf5
John Drigalla <johndylan.drigalla@otto.de>
2026-05-23 03:12:02 +0200

    edit next config ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    basePath: "/JohnManjeetSeed",
+    assetPrefix: "/JohnManjeetSeed/",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
